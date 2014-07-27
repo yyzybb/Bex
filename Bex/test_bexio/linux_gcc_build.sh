8fc3b2df15ab17b5f445d481896298a8ae12afcb
YuYangZi <289633152@qq.com>
2014-07-27 08:19:47 -0700

    test_bexio linux-build

---
+++ Bex/test_bexio/linux_gcc_build.sh
@@ -0,0 +1,1 @@
+g++ main.cpp -std=c++11 -I"../src" -I"../../boost/include" -L"../../boost/lib" -l"boost_system" -l"boost_thread" -l"boost_date_time" -l"pthread" -L"/usr/lib/i386-linux-gnu" -l"gnutls-openssl"
