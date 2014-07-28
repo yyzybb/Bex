88a8e7e1a7bb785024393fbb92750315690719f9
YuYangZi <289633152@qq.com>
2014-07-28 06:19:09 -0700

    linux build

--- Bex/test_bexio/linux_gcc_build.sh
+++ Bex/test_bexio/linux_gcc_build.sh
@@ -1,1 +1,6 @@
-g++ main.cpp -std=c++11 -I"../src" -I"../../boost/include" -L"../../boost/lib" -l"boost_system" -l"boost_thread" -l"boost_date_time" -l"pthread" -L"/usr/lib/i386-linux-gnu" -l"gnutls-openssl"
+#g++ -c main.cpp -o main.o -std=c++11 -I"../src" -I"../../boost/include" -L"/usr/lib/i386-linux-gnu" -l"gnutls-openssl" -l"ssl" -l"crypto" -l"pthread"
+#g++ main.o "../../boost/lib/libboost_system.a" "../../boost/lib/libboost_thread.a" "../../boost/lib/libboost_date_time.a" -static -l"pthread"
+
+g++ -c main.cpp -o main.o -std=c++11 -I"../src" -I"../../boost/include" 
+g++ main.o "../../boost/lib/libboost_system.a" "../../boost/lib/libboost_thread.a" "../../boost/lib/libboost_date_time.a" -l"pthread" -L"/usr/lib/i386-linux-gnu" -l"gnutls-openssl" -l"ssl" -l"crypto" -l"pthread" -o test_bexio.out
+
